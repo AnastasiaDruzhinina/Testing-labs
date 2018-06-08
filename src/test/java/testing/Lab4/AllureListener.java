d4def880ff77e3d7fb19677f3e36f2c636a2303f
Анастасия <nastik.2107@mail.ru>
2018-06-08 19:58:54 +0300

    Lab 4

--- src/test/java/testing/Lab4/AllureListener.java
+++ src/test/java/testing/Lab4/AllureListener.java
@@ -26,4 +26,9 @@ public class AllureListener extends TestListenerAdapter {
     public void onTestFailure(ITestResult iTestResult) {
         makeScreenshot();
     }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        makeScreenshot();
+    }
 }
